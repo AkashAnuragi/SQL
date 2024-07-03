79e461f2810fdf6482e430d3d752e645aaa49c3e
AKASH ANURAGI <136264753+AkashAnuragi@users.noreply.github.com>
2024-07-03 08:54:52 +0530

    20
    1148. Article Views I
    
    Table: Views
    
    +---------------+---------+
    | Column Name   | Type    |
    +---------------+---------+
    | article_id    | int     |
    | author_id     | int     |
    | viewer_id     | int     |
    | view_date     | date    |
    +---------------+---------+
    There is no primary key (column with unique values) for this table, the table may have duplicate rows.
    Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
    Note that equal author_id and viewer_id indicate the same person.
     
    
    Write a solution to find all the authors that viewed at least one of their own articles.
    
    Return the result table sorted by id in ascending order.
    
    The result format is in the following example.
    
     
    
    Example 1:
    
    Input: 
    Views table:
    +------------+-----------+-----------+------------+
    | article_id | author_id | viewer_id | view_date  |
    +------------+-----------+-----------+------------+
    | 1          | 3         | 5         | 2019-08-01 |
    | 1          | 3         | 6         | 2019-08-02 |
    | 2          | 7         | 7         | 2019-08-01 |
    | 2          | 7         | 6         | 2019-08-02 |
    | 4          | 7         | 1         | 2019-07-22 |
    | 3          | 4         | 4         | 2019-07-21 |
    | 3          | 4         | 4         | 2019-07-21 |
    +------------+-----------+-----------+------------+
    Output: 
    +------+
    | id   |
    +------+
    | 4    |
    | 7    |
    +------+

---
+++ 20_Leetcode_problem04.sql
@@ -0,0 +1,37 @@
+SQL> desc Views
+ Name                                      Null?    Type
+ ----------------------------------------- -------- ----------------------------
+ ARTICLE_ID                                         NUMBER(10)
+ AUTHOR_ID                                          NUMBER(10)
+ VIEWER_ID                                          NUMBER(10)
+ VIEW_DATE                                          DATE
+
+SQL> select * from Views;
+
+ARTICLE_ID  AUTHOR_ID  VIEWER_ID VIEW_DATE                                      
+---------- ---------- ---------- ---------                                      
+         1          3          5 01-AUG-19                                      
+         1          3          6 02-AUG-19                                      
+         2          7          7 01-AUG-19                                      
+         2          7          6 02-AUG-19                                      
+         4          7          1 22-JUL-19                                      
+         3          4          4 21-JUL-19                                      
+         3          4          4 21-JUL-19                                      
+
+7 rows selected.
+
+SQL> --solution.
+SQL> select  distinct author_id as id from Views
+  2  where author_id = viewer_id
+  3  order by author_id asc;
+
+        ID                                                                      
+----------                                                                      
+         4                                                                      
+         7                                                                      
+
+SQL> commit;
+
+Commit complete.
+
+SQL> spool off;
